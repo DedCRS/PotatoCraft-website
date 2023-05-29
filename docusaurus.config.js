f2375c421823581341ea82037047ad081b273b45
yukonisen <whenten99@gmail.com>
2023-05-29 18:12:39 +0800

    fix brokenlinks throw

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'yukonisen', // Usually your GitHub org/user name.
   projectName: 'PotatoNetwork', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'log',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
